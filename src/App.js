fedb5ad575d2179c0c0546b22af4d3e3f14981f3
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-08-30 17:54:14 -0400

    Create Navbar component and add to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class Navbar extends React.Component {
-    
+    render() {
+        return(
+            <nav className="nav-wrapper">
+                <div className="container">
+                    <Link to="/" className="brand-logo">Shopping</Link>
+
+                    <ul className="menu">
+                        <li><Link to="/">Shop</Link></li>
+                        <li><Link to="/cart">My Cart</Link></li>
+                        <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
 }
 
 export default Navbar;
