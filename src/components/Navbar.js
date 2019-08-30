3074f2829d2f1833afa77fc92afba2db87b444ec
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-08-30 18:04:33 -0400

    Fix Navbar.js
    Add HashRouter, NavLink, and Route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { 
+    HashRouter,
+    Link,
+    NavLink,
+    Route
+} from 'react-router-dom';
 
 class Navbar extends React.Component {
     render() {
         return(
-            <nav className="nav-wrapper">
-                <div className="container">
-                    <Link to="/" className="brand-logo">Shopping</Link>
+            <HashRouter>
+                <nav className="nav-wrapper">
+                    <div className="container">
+                        <Link to="/" className="brand-logo">Shopping</Link>
 
-                    <ul className="menu">
-                        <li><Link to="/">Shop</Link></li>
-                        <li><Link to="/cart">My Cart</Link></li>
-                        <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
-                    </ul>
-                </div>
-            </nav>
+                        <ul className="menu">
+                            <li><NavLink to="/">Shop</NavLink></li>
+                            <li><NavLink to="/cart">My Cart</NavLink></li>
+                            <li><NavLink to="/cart"><i className="material-icons">shopping_cart</i></NavLink></li>
+                        </ul>
+                    </div>
+                </nav>
+            </HashRouter>
         )
     }
 }
