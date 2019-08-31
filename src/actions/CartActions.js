b4973c3dd1d598f1e842623fbd71f2f2735ddd18
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-08-31 17:29:07 -0400

    Create CartActions.js

---
+++ src/actions/CartActions.js
@@ -0,0 +1,15 @@
+import { ADD_TO_CART } from '../constants/ActionTypes';
+
+export const addToCart = (id) => {
+    return {
+        type: ADD_TO_CART,
+        id
+    }
+}
+
+export const removeItem = 
+
+export const addQuantity = 
+
+export const minusQuantity = 
+
