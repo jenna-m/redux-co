b0aa104ccd210fcf36b5c297d198e60869566faa
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-08-31 17:35:10 -0400

    Update ActionTypes.js

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -1,1 +1,7 @@
 export const ADD_TO_CART = 'ADD_TO_CART';
+
+export const ADD_QUANTITY = 'ADD_QUANTITY';
+
+export const MINUS_QUANTITY = 'MINUS_QUANTITY';
+
+export const REMOVE_ITEM = 'REMOVE_ITEM';
