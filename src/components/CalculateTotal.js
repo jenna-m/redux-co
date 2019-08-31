69fce1f5059a73130b203ebd852f21e674b99080
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-08-31 18:30:14 -0400

    Create CalculateTotal.js
    Add logic to add/remove shipping cost and checkout button

---
+++ src/components/CalculateTotal.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import {
+    ADD_SHIPPING,
+    SUBTRACT_SHIPPING
+} from '../constants/ActionTypes';
+
+const mapStateToProps = (state) => {
+    return {
+        addedItems: state.addedItems,
+        total: state.total
+    }
+}
+
+const dispatchStateToProps = (dispatch) => {
+    return {
+        addShipping: () => {
+            dispatch({type: ADD_SHIPPING})
+        },
+        subtractShipping: () => {
+            dispatch({type: SUBTRACT_SHIPPING})
+        }
+    }
+}
+
+class CalculateTotal extends React.Component {
+    handleChecked = (e) => {
+        if (e.target.checked) {
+            this.props.addShipping();
+        } else {
+            this.props.subtractShipping();
+        }
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="collection">
+                    <li className="collection-item">
+                        <label>
+                            <input type="checkbox" ref="shipping" onChange={this.handleChecked} />
+                            <span>Shipping ($10)</span>
+                        </label>
+                    </li>
+                    <li className="collection-item">
+                        <p>Total: ${this.props.total}</p>
+                    </li>
+                </div>
+
+                <div className="checkout">
+                    <button className="checkout-btn">Checkout</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default connect(mapStateToProps, dispatchStateToProps)(CalculateTotal);
