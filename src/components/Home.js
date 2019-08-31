86dd29c6e247aaa38ee344deb569f558b03283b7
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-08-31 15:07:38 -0400

    Add logic to display items

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,9 +9,29 @@ const mapStateToProps = (state) => {
 
 class Home extends React.Component {
     render() {
+        let itemList = this.props.items.map(item =>{
+            return (
+                <div className="item-card" key={item.id}>
+                    <div className="item-image">
+                        <img src={item.img} alt={item.title} />
+                        <span className="item-title">{item.title}</span>
+                        <span className="add-item">+</span>
+                    </div>
+                    
+                    <div className="card-component">
+                        <p>{item.desc}</p>
+                        <p><b>${item.price}</b></p>
+                    </div>
+                </div>
+            )
+        })
+
         return (
-            <dlv className="container">
-                <h3>Home</h3>
+            <dlv className="new-arrivals">
+                <h3>New Arrivals</h3>
+                <div className="new-arrivals-items">
+                    {itemList}
+                </div>
             </dlv>
         )
     }
