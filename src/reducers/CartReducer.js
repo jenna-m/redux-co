14688b8786b064f0c6af0a44743e2a1f9d038265
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-08-31 10:42:43 -0400

    Add items to CartReducer

---
+++ src/reducers/CartReducer.js
@@ -0,0 +1,61 @@
+import Item1 from '../../images/item1.jpg';
+import Item2 from '../../images/item2.jpg';
+import Item3 from '../../images/item3.jpg';
+import Item4 from '../../images/item4.jpg';
+import Item5 from '../../images/item5.jpg';
+import Item6 from '../../images/item6.jpg';
+
+const initState = {
+    items: [
+        { 
+            id: 1, 
+            title: 'Black Eyelet Dress', 
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.',
+            price: 120,
+            img: Item1
+        },
+        {
+            id: 2,
+            title: 'Halliday Sunglasses',
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.',
+            price: 30,
+            img: Item2
+        },
+        {
+            id: 3,
+            title: 'Boardwalk Sandals',
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.',
+            price: 90,
+            img: Item3
+        },
+        {
+            id: 4,
+            title: 'Chore Coat',
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.',
+            price: 110,
+            img: Item4
+        },
+        {
+            id: 5,
+            title: 'Sunset Crossbody Bag',
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.',
+            price: 90,
+            img: Item5
+        },
+        {
+            id: 6,
+            title: 'Perfect Shorts',
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.',
+            price: 70,
+            img: Item6
+        }
+    ],
+    addedItems: [],
+    total: 0
+}
+
+const cartReducer = (state = initState, action) => {
+    return state;
+}
+
+export default cartReducer;
