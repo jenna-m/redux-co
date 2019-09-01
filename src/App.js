3409a50a93cfed77e6b572ecea8e12975fdcf8ba
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-01 12:43:27 -0400

    Update App.js
    Implement Home and Cart components with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Cart from './components/Cart';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <Home/>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/cart" component={Cart} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
