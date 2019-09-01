0237d9a61281614ab1fe2adbb9186ce78b221107
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-01 16:44:01 -0400

    Update ActionTypes.js
    Add variable for shipping cost

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -9,3 +9,6 @@ export const REMOVE_ITEM = 'REMOVE_ITEM';
 export const ADD_SHIPPING = 'ADD_SHIPPING';
 
 export const SUBTRACT_SHIPPING = 'SUBTRACT_SHIPPING';
+
+// Const for shipping cost
+export const shippingCost = 10;
