35323f79a4066ed102735740263a3bbcddf0a35f
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-01 15:35:02 -0400

    Update Navbar.js
    Fix to make navigation menu functional

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import { 
-    HashRouter,
-    NavLink,
-} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 class Navbar extends React.Component {
     render() {
         return(
-            <HashRouter>
-                <nav className="navbar">
-                    <div className="nav-container">
-                        <NavLink exact to="/" className="brand-logo">Shopping App</NavLink>
-
-                        <ul className="menu">
-                            <li><NavLink exact to="/">Shop</NavLink></li>
-                            <li><NavLink exact to="/cart">My Cart</NavLink></li>
-                            <li><NavLink exact to="/cart"><i class="fa fa-shopping-cart"></i></NavLink></li>
-                        </ul>
-                    </div>
-                </nav>
-            </HashRouter>
+            <nav className="navbar">
+                <div className="nav-container">
+                    <NavLink exact to="/" className="brand-logo">Shopping App</NavLink>
+                    <ul className="menu">
+                        <li><NavLink exact to="/">Shop</NavLink></li>
+                        <li><NavLink to="/cart">My Cart</NavLink></li>
+                        <li class="cart-icon"><NavLink to="/cart"><i class="fa fa-shopping-cart"></i></NavLink></li>
+                    </ul>
+                </div>
+            </nav>
         )
     }
 }
