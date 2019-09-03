363a1f0f6f3718aae3c6929aa49e329369580dd1
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-03 14:07:08 -0400

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="footer">
+                    <div classname="social">
+                        <ul>
+                            <li className="heading">Connect w/ Us</li>
+                            <li><a href="/"><i class="fa fa-facebook-f"></i> Facebook</a></li>
+                            <li><a href="/"><i class="fa fa-instagram"></i> Instagram</a></li>
+                            <li><a href="/"><i class="fa fa-twitter"></i> Twitter</a></li>
+                            <li><a href="/"><i class="fa fa-pinterest-p"></i> Pinterest</a></li>
+                        </ul>
+                    </div>
+                    <div className="location-box">
+                        <div className="location-heading">
+                            <span className="compass-ico"><i class="fa fa-compass"></i></span>
+                            <div className="address">
+                                <p>Pacific Place</p>
+                                <p>600 Pine Street</p>
+                                <p>Seattle, WA 98101</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="shop-links">
+                        <ul>
+                            <li className="heading">Can We Help?</li>
+                            <li><a href="/">Order Status</a></li>
+                            <li><a href="/">Gift Cards</a></li>
+                            <li><a href="/">Returns & Exchanges</a></li>
+                            <li><a href="/">Contact Us</a></li>
+                        </ul>
+                        
+                        
+                    </div>
+                </div>
+
+                <div className="github">
+                    <p>Built with <span className="love-ico"><i class="fa fa-heart"></i></span> by <a href="https://github.com/thesemdrnsocks">thesemdrnsocks</a>. Hosted on <span className="github-ico"><i class="fa fa-github"></i></span>.</p>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default Footer;
