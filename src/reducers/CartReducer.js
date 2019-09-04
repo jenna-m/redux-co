416b061f77cfc0f6d762dd639b1f33da2e6caf0a
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-04 09:20:00 -0400

    Update CartReducer.js
    Import ShopContent.js

--- src/reducers/CartReducer.js
+++ src/reducers/CartReducer.js
@@ -1,15 +1,4 @@
-import Item1 from '../images/item1.png';
-import Item2 from '../images/item2.png';
-import Item3 from '../images/item3.png';
-import Item4 from '../images/item4.png';
-import Item5 from '../images/item5.png';
-import Item6 from '../images/item6.png';
-import Item7 from '../images/item7.png';
-import Item8 from '../images/item8.png';
-import Item9 from '../images/item9.png';
-import Item10 from '../images/item10.png';
-import Item11 from '../images/item11.png';
-import Item12 from '../images/item12.png';
+import ShopContent from '../constants/ShopContent';
 import { 
     ADD_TO_CART,
     REMOVE_ITEM, 
@@ -22,92 +11,7 @@ import {
 } from '../constants/ActionTypes';
 
 const initState = {
-    items: [
-        { 
-            id: 1, 
-            title: 'Black Eyelet Dress', 
-            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            price: 120,
-            img: Item1
-        },
-        {
-            id: 2,
-            title: 'Halliday Sunglasses',
-            desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-            price: 30,
-            img: Item2
-        },
-        {
-            id: 3,
-            title: 'Black Chelsea Boots',
-            desc: 'Posuere morbi leo urna molestie at elementum eu facilisis sed. At auctor urna nunc id cursus metus. Eu consequat ac felis donec et odio pellentesque.',
-            price: 120,
-            img: Item3
-        },
-        {
-            id: 4,
-            title: 'Chore Coat',
-            desc: 'Dui sapien eget mi proin sed libero. Pharetra pharetra massa massa ultricies. Nisi scelerisque eu ultrices vitae auctor eu augue. Blandit cursus risus at ultrices.',
-            price: 110,
-            img: Item4
-        },
-        {
-            id: 5,
-            title: 'Sunset Crossbody Bag',
-            desc: 'Eget nunc lobortis mattis aliquam faucibus purus. Nibh praesent tristique magna sit amet purus gravida quis.',
-            price: 90,
-            img: Item5
-        },
-        {
-            id: 6,
-            title: 'Perfect Shorts',
-            desc: 'Quisque egestas diam in arcu. Amet risus nullam eget felis. Faucibus a pellentesque sit amet. Morbi leo urna molestie at elementum eu.',
-            price: 70,
-            img: Item6
-        },
-        { 
-            id: 7, 
-            title: 'Boardwalk Sandals', 
-            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            price: 70,
-            img: Item7
-        },
-        {
-            id: 8,
-            title: 'High-Rise Skinny Jeans',
-            desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-            price: 90,
-            img: Item8
-        },
-        {
-            id: 9,
-            title: 'Moon Necklace ',
-            desc: 'Posuere morbi leo urna molestie at elementum eu facilisis sed. At auctor urna nunc id cursus metus. Eu consequat ac felis donec et odio pellentesque.',
-            price: 30,
-            img: Item9
-        },
-        {
-            id: 10,
-            title: 'Button-Down Cami',
-            desc: 'Dui sapien eget mi proin sed libero. Pharetra pharetra massa massa ultricies. Nisi scelerisque eu ultrices vitae auctor eu augue. Blandit cursus risus at ultrices.',
-            price: 40,
-            img: Item10
-        },
-        {
-            id: 11,
-            title: 'Striped Sweater',
-            desc: 'Eget nunc lobortis mattis aliquam faucibus purus. Nibh praesent tristique magna sit amet purus gravida quis.',
-            price: 90,
-            img: Item11
-        },
-        {
-            id: 12,
-            title: 'Denim Mini Dress',
-            desc: 'Quisque egestas diam in arcu. Amet risus nullam eget felis. Faucibus a pellentesque sit amet. Morbi leo urna molestie at elementum eu.',
-            price: 140,
-            img: Item12
-        }
-    ],
+    items: ShopContent,
     addedItems: [],
     total: 0
 }
