600d67bd3ad872c0e2c78d3b8b6a63001aaf2baf
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-04 09:27:18 -0400

    Update ShopContent.js
    Add category and sale keys

--- src/constants/ShopContent.js
+++ src/constants/ShopContent.js
@@ -17,83 +17,107 @@ export const ShopContent = [
         title: 'Black Eyelet Dress', 
         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         price: 120,
-        img: Item1
+        img: Item1,
+        category: 'dresses',
+        sale: false
     },
     {
         id: 2,
         title: 'Halliday Sunglasses',
         desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         price: 30,
-        img: Item2
+        img: Item2,
+        category: 'accessories',
+        sale: false
     },
     {
         id: 3,
         title: 'Black Chelsea Boots',
         desc: 'Posuere morbi leo urna molestie at elementum eu facilisis sed. At auctor urna nunc id cursus metus. Eu consequat ac felis donec et odio pellentesque.',
         price: 120,
-        img: Item3
+        img: Item3,
+        category: 'shoes',
+        sale: false
     },
     {
         id: 4,
         title: 'Chore Coat',
         desc: 'Dui sapien eget mi proin sed libero. Pharetra pharetra massa massa ultricies. Nisi scelerisque eu ultrices vitae auctor eu augue. Blandit cursus risus at ultrices.',
         price: 110,
-        img: Item4
+        img: Item4,
+        category: 'sweaters-jackets',
+        sale: false
     },
     {
         id: 5,
         title: 'Sunset Crossbody Bag',
         desc: 'Eget nunc lobortis mattis aliquam faucibus purus. Nibh praesent tristique magna sit amet purus gravida quis.',
         price: 90,
-        img: Item5
+        img: Item5,
+        category: 'accessories',
+        sale: false
     },
     {
         id: 6,
         title: 'Perfect Shorts',
         desc: 'Quisque egestas diam in arcu. Amet risus nullam eget felis. Faucibus a pellentesque sit amet. Morbi leo urna molestie at elementum eu.',
         price: 70,
-        img: Item6
+        img: Item6,
+        category: 'denim',
+        sale: false
     },
     { 
         id: 7, 
         title: 'Boardwalk Sandals', 
         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         price: 70,
-        img: Item7
+        img: Item7,
+        category: 'shoes',
+        sale: false
     },
     {
         id: 8,
         title: 'High-Rise Skinny Jeans',
         desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         price: 90,
-        img: Item8
+        img: Item8,
+        category: 'denim',
+        sale: false
     },
     {
         id: 9,
         title: 'Moon Necklace ',
         desc: 'Posuere morbi leo urna molestie at elementum eu facilisis sed. At auctor urna nunc id cursus metus. Eu consequat ac felis donec et odio pellentesque.',
         price: 30,
-        img: Item9
+        img: Item9,
+        category: 'accessories',
+        sale: false
     },
     {
         id: 10,
         title: 'Button-Down Cami',
         desc: 'Dui sapien eget mi proin sed libero. Pharetra pharetra massa massa ultricies. Nisi scelerisque eu ultrices vitae auctor eu augue. Blandit cursus risus at ultrices.',
         price: 40,
-        img: Item10
+        img: Item10,
+        category: 'tops',
+        sale: false
     },
     {
         id: 11,
         title: 'Striped Sweater',
         desc: 'Eget nunc lobortis mattis aliquam faucibus purus. Nibh praesent tristique magna sit amet purus gravida quis.',
         price: 90,
-        img: Item11
+        img: Item11,
+        category: 'sweaters-jackets',
+        sale: false
     },
     {
         id: 12,
         title: 'Denim Mini Dress',
         desc: 'Quisque egestas diam in arcu. Amet risus nullam eget felis. Faucibus a pellentesque sit amet. Morbi leo urna molestie at elementum eu.',
         price: 140,
-        img: Item12
+        img: Item12,
+        category: 'dresses',
+        sale: false
     }
 ];
