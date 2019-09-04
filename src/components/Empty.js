5d31ffd751aa793538c9fe72fc79e5fcf9a25e26
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-04 09:12:46 -0400

    Update Empty.js
    Change div class name

--- src/components/Empty.js
+++ src/components/Empty.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Empty extends React.Component {
     render() {
         return (
-            <div className="shopping-message">
+            <div className="empty-cart-message">
                 <p>Nothing</p>
                 <a className="button" href="/">Start shopping!</a>
             </div>
