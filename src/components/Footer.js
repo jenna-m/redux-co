65836411565d0db3fb1397e8e661d305ee6dfd5e
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-04 09:13:03 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,19 +14,19 @@ class Footer extends React.Component {
                             <li><a href="/"><i class="fa fa-pinterest-p"></i> Pinterest</a></li>
                         </ul>
                     </div>
-                    <div className="location-box">
+                    <div className="location">
                         <div className="location-heading">
-                            <span className="compass-ico"><i class="fa fa-compass"></i></span>
-                            <div className="address">
-                                <p>Pacific Place</p>
-                                <p>600 Pine Street</p>
-                                <p>Seattle, WA 98101</p>
-                            </div>
+                            <span><i class="fa fa-compass"></i></span>
+                        </div>
+                        <div className="address">
+                            <p>Pacific Place</p>
+                            <p>600 Pine Street</p>
+                            <p>Seattle, WA 98101</p>
                         </div>
                     </div>
                     <div className="shop-links">
                         <ul>
-                            <li className="heading">Can We Help?</li>
+                            <li className="heading">Any Questions?</li>
                             <li><a href="/">Order Status</a></li>
                             <li><a href="/">Gift Cards</a></li>
                             <li><a href="/">Returns & Exchanges</a></li>
@@ -38,7 +38,7 @@ class Footer extends React.Component {
                 </div>
 
                 <div className="github">
-                    <p>Built with <span className="love-ico"><i class="fa fa-heart"></i></span> by <a href="https://github.com/thesemdrnsocks">thesemdrnsocks</a>. Hosted on <span className="github-ico"><i class="fa fa-github"></i></span>.</p>
+                    <p>Built with <span className="love-ico"><i class="fa fa-heart"></i></span> by <a href="https://github.com/thesemdrnsocks">thesemdrnsocks</a>. Hosted on GitHub.</p>
                 </div>
             </div>
         );
