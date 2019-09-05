65ca26c4c3824f0a0c5ff92a7ab7880690bd954c
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-05 11:30:53 -0400

    Update ActionTypes.js
    Add CHANGE_SIZE action type

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -6,11 +6,12 @@ export const MINUS_QUANTITY = 'MINUS_QUANTITY';
 
 export const REMOVE_ITEM = 'REMOVE_ITEM';
 
+export const CHANGE_SIZE = 'CHANGE_SIZE';
+
 export const ADD_SHIPPING = 'ADD_SHIPPING';
 
 export const SUBTRACT_SHIPPING = 'SUBTRACT_SHIPPING';
 
-// Const for shipping cost
 export const shippingCost = 10;
 
 export const ADD_PICKUP = 'ADD_PICKUP';
