152c5d79aef40f1786addc73b11eb377eda7fdf4
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-05 11:28:01 -0400

    Update App.js
    Change routing for Cart component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Cart from './components/Cart';
+import Cart from './components/cart/Cart';
 import Footer from './components/Footer';
 
 class App extends React.Component {
