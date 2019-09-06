ad65d720c8bd43bfc3c4eaa75f3f3f70fb412029
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-06 07:53:05 -0400

    Update App.js
    Import routes as a variable

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import {
-  BrowserRouter,
+  BrowserRouter as Router,
   Route,
   Switch
 } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Cart from './components/cart/Cart';
-import Footer from './components/Footer';
+import Footer from './components/footer/Footer';
+import { Routes } from './constants/Routes';
+
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <Router>
         <div className="App">
           <Navbar />
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/cart" component={Cart} />
+            { Routes }
           </Switch>
           <Footer />
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
