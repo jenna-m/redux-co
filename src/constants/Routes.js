b95fea684d0dd2287e298a566956750251a03a83
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-06 07:54:41 -0400

    Create Router.js
    Will hold all routes

---
+++ src/constants/Routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Home from '../components/Home';
+import Cart from '../components/cart/Cart';
+import OrderStatus from '../components/footer/OrderStatus';
+import GiftCards from '../components/footer/GiftCards';
+import ReturnsExchanges from '../components/footer/Returns-Exchanges';
+import Contact from '../components/footer/Contact';
+
+export const Routes = <div>
+            <Route exact path="/" component={ Home } />
+            <Route path="/cart" component={ Cart } />
+            <Route path="/order-status" component={ OrderStatus } />
+            <Route path="/gift-cards" component={ GiftCards } />
+            <Route path="/returns-exchanges" component={ ReturnsExchanges } />
+            <Route path="/contact" component={ Contact } />
+            </div>
