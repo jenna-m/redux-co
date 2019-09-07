57b0109223b3b293e0bc324bee70f4ed5d117908
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-07 10:00:30 -0400

    Update index.js
    Wrap App in <Router>

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import cartReducer from './reducers/CartReducer';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import App from './App';
 
 const store = createStore(cartReducer);
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
