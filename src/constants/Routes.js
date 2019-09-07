3e483121091cb78cdba853536e07caec73e04245
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-07 10:00:03 -0400

    Update Routes.js

--- src/constants/Routes.js
+++ src/constants/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from '../components/Home';
+import Item from '../components/shopping/Item';
 import Cart from '../components/cart/Cart';
 import OrderStatus from '../components/footer/OrderStatus';
 import GiftCards from '../components/footer/GiftCards';
@@ -9,6 +10,7 @@ import Contact from '../components/footer/Contact';
 
 export const Routes = <div>
             <Route exact path="/" component={ Home } />
+            <Route path="/products" component={ Item }/>
             <Route path="/cart" component={ Cart } />
             <Route path="/order-status" component={ OrderStatus } />
             <Route path="/gift-cards" component={ GiftCards } />
