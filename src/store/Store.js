7cd68696477c49c6789d3b69a00808cd3333814d
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-08 12:32:48 -0400

    Update Store.js

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,4 +1,11 @@
 import { createStore } from 'redux';
+import ShopContent from '../constants/ShopContent';
 import cartReducer from '../reducers/CartReducer';
 
-export const store = createStore(cartReducer);
+const initState = {
+    items: ShopContent,
+    addedItems: [],
+    total: 0
+}
+
+export const store = createStore(cartReducer, initState);
