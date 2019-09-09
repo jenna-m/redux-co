fd99489b56375ff808e2ff7c88e4ff32d9d0912c
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-09 16:27:55 -0400

    Update GiftCards.js
    Change display of gift card values

--- src/components/footer/GiftCards.js
+++ src/components/footer/GiftCards.js
@@ -6,17 +6,15 @@ class GiftCards extends React.Component {
             <div>
                 <h1>Gift Cards</h1>
                 <h2>Select a gift card amount below to add to your cart.</h2>
-                    <div className="form-content">
-                        <form>
-                            <select name="gift-card-value">
-                                <option value="25">$25.00</option>
-                                <option value="50">$50.00</option>
-                                <option value="75">$75.00</option>
-                                <option value="100">$100.00</option>
-                            </select>
-                            <button href="/">Purchase</button>
-                        </form>
-                    </div>
+                <div className="giftcard-options">
+                    <button value="25">$25.00</button>
+                    <button value="50">$50.00</button>
+                    <button value="75">$75.00</button>
+                    <button value="100">$100.00</button>
+                </div>
+                <div className="giftcard-purchase">
+                    <button href="/">Purchase</button>
+                </div>
             </div>
         );
     }
