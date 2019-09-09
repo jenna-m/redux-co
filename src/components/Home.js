584bf57c57d8ebd347e1d7fd50a28f73b710a54d
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-09 16:28:02 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,6 @@ import {
     Link,
     withRouter
 } from 'react-router-dom';
-import { addToCart } from '../actions/CartActions';
 import Item from './shopping/Item';
 
 const mapStateToProps = (state) => {
@@ -19,16 +18,11 @@ class Home extends React.Component {
             return (
                 <div className="home-item-card" key={item.id}>
                     <div className="home-item-image">
-                        <Link to = 
-                            {{ pathname: `/products/${item.category}`,
-                            search: `?id=${item.id}`,
-                            state: {
-                                id: `${item.id}`,
-                                img: `${item.img}`,
-                                title: `${item.title}`, 
-                                desc: `${item.desc}`,
-                                price: `${item.price}`
-                            } }}
+                        <Link
+                            to = {{
+                                pathname: `/products/${item.category}`,
+                                search: `?id=${item.id}`,
+                            }}
                             component={ Item }>
                                 <img src={item.img} alt={item.title} />
                         </Link>
