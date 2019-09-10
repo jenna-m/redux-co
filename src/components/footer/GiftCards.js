397d57e5eaa3eb27f0f48aea416b09c35a6d45ce
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-10 11:06:50 -0400

    Update GiftCards.js

--- src/components/footer/GiftCards.js
+++ src/components/footer/GiftCards.js
@@ -6,14 +6,16 @@ class GiftCards extends React.Component {
             <div>
                 <h1>Gift Cards</h1>
                 <h2>Select a gift card amount below to add to your cart.</h2>
-                <div className="giftcard-options">
-                    <button value="25">$25.00</button>
-                    <button value="50">$50.00</button>
-                    <button value="75">$75.00</button>
-                    <button value="100">$100.00</button>
-                </div>
-                <div className="giftcard-purchase">
-                    <button href="/">Purchase</button>
+                <div className="giftcard-section">
+                    <div className="giftcard-options">
+                        <button value="25">$25.00</button>
+                        <button value="50">$50.00</button>
+                        <button value="75">$75.00</button>
+                        <button value="100">$100.00</button>
+                    </div>
+                    <div className="giftcard-purchase">
+                        <button href="/">Purchase</button>
+                    </div>
                 </div>
             </div>
         );
