eaaca8038b0f8f3800df87e005f1e71eb0328f5c
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-12 10:33:15 -0400

    Update configureStore.js
    Add persist

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,15 @@
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'; // localStorage for web
 import CartReducer from '../reducers/CartReducer';
 
-export const store = createStore(CartReducer);
+const persistConfig = {
+    key: 'root',
+    storage,
+    whiteList: ['addedItems'] // will be persisted
+};
+
+const pReducer = persistReducer(persistConfig, CartReducer);
+
+export const store = createStore(pReducer);
+export const persistor = persistStore(store);
