772c9b6e4289b9f24edf0214d1b52bf346cdaf44
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-12 10:33:06 -0400

    Update CartReducer.js

--- src/reducers/CartReducer.js
+++ src/reducers/CartReducer.js
@@ -4,11 +4,17 @@ import {
     REMOVE_ITEM, 
     ADD_QUANTITY, 
     MINUS_QUANTITY,
+    ADD_SHIPPING,
+    SUBTRACT_SHIPPING,
+    shippingCost,
+    ADD_PICKUP
 } from '../constants/ActionTypes';
 
 const initState = {
     items: ShopContent,
     addedItems: [],
+    subTotal: 0,
+    cartTotal: 0
 }
 
 const cartReducer = (state = initState, action) => {
@@ -18,31 +24,39 @@ const cartReducer = (state = initState, action) => {
         let existedItem = state.addedItems.find(item => action.id === item.id);
 
         // Check if item already exists in cart
-        // If item is already in cart, increase quantity by 1
+        // If item is already in cart, increase quantity by 1, and calculate subTotal and cartTotal
         if (existedItem) {
             addedItem.quantity += 1;
             return {
-                ...state
+                ...state,
+                subTotal: state.subTotal + addedItem.price,
+                cartTotal: state.subTotal + addedItem.price + shippingCost
             }
         } else {
-            // Add item to cart
+            // Add item to cart and calculate subTotal and cartTotal
             addedItem.quantity = 1;
+            let newSubTotal = state.subTotal + addedItem.price;
             return {
                 ...state,
-                addedItems: [...state.addedItems, addedItem]
+                addedItems: [...state.addedItems, addedItem],
+                subTotal: newSubTotal,
+                cartTotal: newSubTotal + shippingCost
             }
         }
     }
 
-    //If item is removed from cart, remove item
+    //If item is removed from cart, remove item and recalculate subTotal and cartTotal
     if (action.type === REMOVE_ITEM) {
         let itemToRemove = state.addedItems.find(item => action.id === item.id);
         let newItems = state.addedItems.filter(item => action.id !== item.id);
 
+        let newSubTotal = state.subTotal - (itemToRemove.price * itemToRemove.quantity);
         console.log(itemToRemove);
         return {
             ...state,
-            addedItems: newItems
+            addedItems: newItems,
+            subTotal: newSubTotal,
+            cartTotal: newSubTotal + shippingCost
         }
     }
     
@@ -52,8 +66,11 @@ const cartReducer = (state = initState, action) => {
         
         addedItem.quantity += 1;
 
+        let newSubTotal = state.subTotal + addedItem.price;
         return {
-            ...state
+            ...state,
+            subTotal: newSubTotal,
+            cartTotal: newSubTotal + shippingCost
         }
 
     }
@@ -63,15 +80,50 @@ const cartReducer = (state = initState, action) => {
         let addedItem = state.items.find(item => item.id === action.id);
         if (addedItem.quantity === 1) {
             let newItems = state.addedItems.filter(item => item.id !== action.id);
+            let newSubTotal = state.subTotal - addedItem.price;
             return {
                 ...state,
-                addedItems: newItems
+                addedItems: newItems,
+                subTotal: newSubTotal,
+                cartTotal: newSubTotal + shippingCost
             }
         } else {
             addedItem.quantity -= 1;
+            let newSubTotal = state.subTotal - addedItem.price;
             return {
-                ...state
+                ...state,
+                subTotal: newSubTotal,
+                cartTotal: newSubTotal + shippingCost
             }
+        }
+    }
+
+    // Logic to add and subtract shipping costs
+    if (action.type === ADD_SHIPPING) {
+        return {
+            ...state,
+            subTotal: state.subTotal,
+            shipping: shippingCost,
+            cartTotal: state.subTotal + shippingCost
+        }
+    }
+
+    if (action.type === SUBTRACT_SHIPPING) {
+        return {
+            ...state,
+            subTotal: state.subTotal,
+            shipping: 0,
+            cartTotal: state.subTotal - shippingCost
+        }
+    }
+
+    // Logic for in-store pickup
+    if (action.type === ADD_PICKUP) {
+        return {
+            ...state,
+            subTotal: state.subTotal,
+            shipping: 0,
+            cartTotal: state.subTotal
         }
     }
     
