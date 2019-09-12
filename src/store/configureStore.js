f8ee12e97be1acf50aacadef649502bd050003ff
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-12 09:58:18 -0400

    Update configureStore.js
    Remove persist

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,4 @@
 import { createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'; // localStorage for web
 import CartReducer from '../reducers/CartReducer';
 
-const persistConfig = {
-    key: 'root',
-    storage
-};
-
-const pReducer = persistReducer(persistConfig, CartReducer);
-
-export const store = createStore(pReducer);
-export const persistor = persistStore(store);
+export const store = createStore(CartReducer);
