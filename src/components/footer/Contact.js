76d3e5d8531e7323776496f942fdcce28d2eac87
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-13 11:54:05 -0400

    Update Contact.js
    Begin to build contact form

--- src/components/footer/Contact.js
+++ src/components/footer/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 class Contact extends React.Component {
     render() {
@@ -9,10 +10,40 @@ class Contact extends React.Component {
                     <h2>Do you have questions, comments, or concerns?</h2>
                     <h3>Contact us with the form below.</h3>
                     <div className="form-content">
-                        <form>
-                            <input type="text" />
-                            <button href="/">Submit</button>
-                        </form>
+                        <Formik
+                            initialValies = {{ name: '', email: '', message: '' }}
+                            validate = { values => {
+                                let errors = {};
+                                if (!values.email) {
+                                    errors.email = 'Required';
+                                } else if (
+                                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                                ) {
+                                    errors.email = 'Invalid email address';
+                                }
+                                return errors;
+                                }}
+                                onSubmit={(values, { setSubmitting }) => {
+                                    setTimeout(() => {
+                                        alert(JSON.stringify(values, null, 2));
+                                        setSubmitting(false);
+                                    }, 400)
+                                }}
+                            >
+                                {({ isSubmitting }) => (
+                                    <Form>
+                                        <Field type="name" name="name" />
+                                        <ErrorMessage name="name" component="div" />
+                                        <Field type="email" name="email" />
+                                        <ErrorMessage name="email" component="div" />
+                                        <Field type="message" name="message" />
+                                        <ErrorMessage name="message" component="div" />
+                                        <button type="submit" disabled={isSubmitting}>
+                                            Submit
+                                        </button>
+                                    </Form>
+                                )}
+                        </Formik>
                     </div>
                 </div>
             </div>
