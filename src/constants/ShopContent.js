2ad485effbde0dbed6dcb2a4bfe58f6247784e2d
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-17 18:22:13 -0400

    Upload new items

--- src/constants/ShopContent.js
+++ src/constants/ShopContent.js
@@ -1,15 +1,46 @@
-import Item1 from '../images/item1.png';
-import Item2 from '../images/item2.png';
-import Item3 from '../images/item3.png';
-import Item4 from '../images/item4.png';
-import Item5 from '../images/item5.png';
-import Item6 from '../images/item6.png';
-import Item7 from '../images/item7.png';
-import Item8 from '../images/item8.png';
-import Item9 from '../images/item9.png';
-import Item10 from '../images/item10.png';
-import Item11 from '../images/item11.png';
-import Item12 from '../images/item12.png';
+import Item1 from '../images/Item1.jpg';
+import Item2 from '../images/Item2.jpg';
+import Item3 from '../images/Item3.jpg';
+import Item4 from '../images/Item4.jpg';
+import Item5 from '../images/Item5.jpg';
+import Item6 from '../images/Item6.jpg';
+import Item7 from '../images/Item7.jpg';
+import Item8 from '../images/Item8.jpg';
+import Item9 from '../images/Item9.jpg';
+import Item10 from '../images/Item10.jpg';
+import Item11 from '../images/Item11.jpg';
+import Item12 from '../images/Item12.jpg';
+import Item13 from '../images/Item13.jpg';
+import Item14 from '../images/Item14.jpg';
+import Item15 from '../images/Item15.jpg';
+import Item16 from '../images/Item16.jpg';
+import Item17 from '../images/Item17.jpg';
+import Item18 from '../images/Item18.jpg';
+import Item19 from '../images/Item19.jpg';
+import Item20 from '../images/Item20.jpg';
+import Item21 from '../images/Item21.jpg';
+import Item22 from '../images/Item22.jpg';
+import Item23 from '../images/Item23.jpg';
+import Item24 from '../images/Item24.jpg';
+import Item25 from '../images/Item25.jpg';
+import Item26 from '../images/Item26.jpg';
+import Item27 from '../images/Item27.jpg';
+import Item28 from '../images/Item28.jpg';
+import Item29 from '../images/Item29.jpg';
+import Item30 from '../images/Item30.jpg';
+import Item31 from '../images/Item31.jpg';
+import Item32 from '../images/Item32.jpg';
+import Item33 from '../images/Item33.jpg';
+import Item34 from '../images/Item34.jpg';
+import Item35 from '../images/Item35.jpg';
+import Item36 from '../images/Item36.jpg';
+import Item37 from '../images/Item37.jpg';
+import Item38 from '../images/Item38.jpg';
+import Item39 from '../images/Item39.jpg';
+import Item40 from '../images/Item40.jpg';
+import Item41 from '../images/Item41.jpg';
+import Item42 from '../images/Item42.jpg';
+
 
 export const ShopContent = [
     { 
@@ -26,7 +57,7 @@ export const ShopContent = [
         id: 2,
         title: 'Halliday Sunglasses',
         desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-        price: 30,
+        price: 40,
         img: Item2,
         category: 'accessories',
         subcat: 'eyewear',
@@ -34,7 +65,7 @@ export const ShopContent = [
     },
     {
         id: 3,
-        title: 'Black Chelsea Boots',
+        title: 'Ainsley Chelsea Boots',
         desc: 'Posuere morbi leo urna molestie at elementum eu facilisis sed. At auctor urna nunc id cursus metus. Eu consequat ac felis donec et odio pellentesque.',
         price: 120,
         img: Item3,
@@ -54,7 +85,7 @@ export const ShopContent = [
     },
     {
         id: 5,
-        title: 'Sunset Crossbody Bag',
+        title: 'Simple Crossbody Bag',
         desc: 'Eget nunc lobortis mattis aliquam faucibus purus. Nibh praesent tristique magna sit amet purus gravida quis.',
         price: 90,
         img: Item5,
@@ -64,7 +95,7 @@ export const ShopContent = [
     },
     {
         id: 6,
-        title: 'Perfect Shorts',
+        title: 'Perfect Jean Shorts in Black',
         desc: 'Quisque egestas diam in arcu. Amet risus nullam eget felis. Faucibus a pellentesque sit amet. Morbi leo urna molestie at elementum eu.',
         price: 70,
         img: Item6,
@@ -74,9 +105,9 @@ export const ShopContent = [
     },
     { 
         id: 7, 
-        title: 'Boardwalk Sandals', 
+        title: 'Post Slide Sandals', 
         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        price: 70,
+        price: 50,
         img: Item7,
         category: 'shoes',
         subcat: 'sandals',
@@ -84,7 +115,7 @@ export const ShopContent = [
     },
     {
         id: 8,
-        title: 'High-Rise Skinny Jeans',
+        title: 'Button-Through Skinny Jeans',
         desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         price: 90,
         img: Item8,
@@ -94,7 +125,7 @@ export const ShopContent = [
     },
     {
         id: 9,
-        title: 'Moon Necklace ',
+        title: 'Moon Charm Necklace',
         desc: 'Posuere morbi leo urna molestie at elementum eu facilisis sed. At auctor urna nunc id cursus metus. Eu consequat ac felis donec et odio pellentesque.',
         price: 30,
         img: Item9,
@@ -104,7 +135,7 @@ export const ShopContent = [
     },
     {
         id: 10,
-        title: 'Button-Down Cami',
+        title: 'Button-Down Silk Cami',
         desc: 'Dui sapien eget mi proin sed libero. Pharetra pharetra massa massa ultricies. Nisi scelerisque eu ultrices vitae auctor eu augue. Blandit cursus risus at ultrices.',
         price: 40,
         img: Item10,
@@ -114,9 +145,9 @@ export const ShopContent = [
     },
     {
         id: 11,
-        title: 'Striped Sweater',
+        title: 'Bobble Pullover Sweater',
         desc: 'Eget nunc lobortis mattis aliquam faucibus purus. Nibh praesent tristique magna sit amet purus gravida quis.',
-        price: 90,
+        price: 100,
         img: Item11,
         category: 'clothing',
         subcat: 'sweaters',
@@ -124,12 +155,312 @@ export const ShopContent = [
     },
     {
         id: 12,
-        title: 'Denim Mini Dress',
+        title: 'Raw Hemline Mini Skirt',
         desc: 'Quisque egestas diam in arcu. Amet risus nullam eget felis. Faucibus a pellentesque sit amet. Morbi leo urna molestie at elementum eu.',
-        price: 140,
+        price: 70,
         img: Item12,
         category: 'clothing',
+        subcat: 'skirts',
+        sale: false
+    },
+    { 
+        id: 13, 
+        title: 'Paperbag Denim Midi Skirt', 
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        price: 90,
+        img: Item13,
+        category: 'clothing',
+        subcat: 'skirts',
+        sale: false
+    },
+    {
+        id: 14,
+        title: 'Posy Print Midi Dress',
+        desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        price: 130,
+        img: Item14,
+        category: 'clothing',
         subcat: 'dresses',
+        sale: false
+    },
+    {
+        id: 15,
+        title: 'Gingham Midi Dress',
+        desc: 'Posuere morbi leo urna molestie at elementum eu facilisis sed. At auctor urna nunc id cursus metus. Eu consequat ac felis donec et odio pellentesque.',
+        price: 120,
+        img: Item15,
+        category: 'clothing',
+        subcat: 'dresses',
+        sale: false
+    },
+    {
+        id: 16,
+        title: 'Cement Wash Skinny Jeans',
+        desc: 'Dui sapien eget mi proin sed libero. Pharetra pharetra massa massa ultricies. Nisi scelerisque eu ultrices vitae auctor eu augue. Blandit cursus risus at ultrices.',
+        price: 90,
+        img: Item16,
+        category: 'clothing',
+        subcat: 'denim',
+        sale: false
+    },
+    {
+        id: 17,
+        title: 'Backyard Blooms Midi Dress',
+        desc: 'Eget nunc lobortis mattis aliquam faucibus purus. Nibh praesent tristique magna sit amet purus gravida quis.',
+        price: 110,
+        img: Item17,
+        category: 'clothing',
+        subcat: 'dresses',
+        sale: false
+    },
+    {
+        id: 18,
+        title: 'Perfect Fit Vintage Jeans',
+        desc: 'Quisque egestas diam in arcu. Amet risus nullam eget felis. Faucibus a pellentesque sit amet. Morbi leo urna molestie at elementum eu.',
+        price: 120,
+        img: Item18,
+        category: 'clothing',
+        subcat: 'denim',
+        sale: false
+    },
+    { 
+        id: 19, 
+        title: 'Classic Straight Jeans', 
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        price: 120,
+        img: Item19,
+        category: 'clothing',
+        subcat: 'denim',
+        sale: false
+    },
+    {
+        id: 20,
+        title: 'Ex-Boyfriend Shirt in Seconda Plaid',
+        desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        price: 90,
+        img: Item20,
+        category: 'clothing',
+        subcat: 'tops',
+        sale: false
+    },
+    {
+        id: 21,
+        title: 'Northside Vintage Tee',
+        desc: 'Posuere morbi leo urna molestie at elementum eu facilisis sed. At auctor urna nunc id cursus metus. Eu consequat ac felis donec et odio pellentesque.',
+        price: 25,
+        img: Item21,
+        category: 'clothing',
+        subcat: 'tops',
+        sale: false
+    },
+    {
+        id: 22,
+        title: 'Textured Tee in Stripe',
+        desc: 'Dui sapien eget mi proin sed libero. Pharetra pharetra massa massa ultricies. Nisi scelerisque eu ultrices vitae auctor eu augue. Blandit cursus risus at ultrices.',
+        price: 30,
+        img: Item22,
+        category: 'clothing',
+        subcat: 'tops',
+        sale: false
+    },
+    {
+        id: 23,
+        title: 'Sherpa-Lined Swing Coat',
+        desc: 'Eget nunc lobortis mattis aliquam faucibus purus. Nibh praesent tristique magna sit amet purus gravida quis.',
+        price: 140,
+        img: Item23,
+        category: 'clothing',
+        subcat: 'jackets',
+        sale: false
+    },
+    {
+        id: 24,
+        title: 'Ex-Boyfriend Cardigan',
+        desc: 'Quisque egestas diam in arcu. Amet risus nullam eget felis. Faucibus a pellentesque sit amet. Morbi leo urna molestie at elementum eu.',
+        price: 80,
+        img: Item24,
+        category: 'clothing',
+        subcat: 'sweaters',
+        sale: false
+    },
+    {
+        id: 25,
+        title: 'High-Rise Denim Shorts',
+        desc: 'Quisque egestas diam in arcu. Amet risus nullam eget felis. Faucibus a pellentesque sit amet. Morbi leo urna molestie at elementum eu.',
+        price: 70,
+        img: Item25,
+        category: 'clothing',
+        subcat: 'denim',
+        sale: false
+    },
+    { 
+        id: 26, 
+        title: 'Kent Striped Cardigan', 
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        price: 130,
+        img: Item26,
+        category: 'clothing',
+        subcat: 'sweaters',
+        sale: false
+    },
+    {
+        id: 27,
+        title: 'Cashmere Sweatshirt',
+        desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        price: 100,
+        img: Item27,
+        category: 'clothing',
+        subcat: 'sweaters',
+        sale: false
+    },
+    {
+        id: 28,
+        title: 'Buttoned Chambray Midi Skirt',
+        desc: 'Posuere morbi leo urna molestie at elementum eu facilisis sed. At auctor urna nunc id cursus metus. Eu consequat ac felis donec et odio pellentesque.',
+        price: 90,
+        img: Item28,
+        category: 'clothing',
+        subcat: 'skirts',
+        sale: false
+    },
+    {
+        id: 29,
+        title: 'Paulette Sandals',
+        desc: 'Dui sapien eget mi proin sed libero. Pharetra pharetra massa massa ultricies. Nisi scelerisque eu ultrices vitae auctor eu augue. Blandit cursus risus at ultrices.',
+        price: 140,
+        img: Item29,
+        category: 'shoes',
+        subcat: 'heels',
+        sale: false
+    },
+    {
+        id: 30,
+        title: 'Allie Knee High Boots',
+        desc: 'Eget nunc lobortis mattis aliquam faucibus purus. Nibh praesent tristique magna sit amet purus gravida quis.',
+        price: 190,
+        img: Item30,
+        category: 'shoes',
+        subcat: 'boots',
+        sale: false
+    },
+    {
+        id: 31,
+        title: 'Basket Hoop Earrings',
+        desc: 'Quisque egestas diam in arcu. Amet risus nullam eget felis. Faucibus a pellentesque sit amet. Morbi leo urna molestie at elementum eu.',
+        price: 25,
+        img: Item31,
+        category: 'accessories',
+        subcat: 'jewelry',
+        sale: false
+    },
+    { 
+        id: 32, 
+        title: 'Stained Glass Ring Set', 
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        price: 30,
+        img: Item32,
+        category: 'accessories',
+        subcat: 'jewelry',
+        sale: false
+    },
+    {
+        id: 33,
+        title: 'Transport Bucket Bag in Leather',
+        desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        price: 120,
+        img: Item33,
+        category: 'accessories',
+        subcat: 'purses',
+        sale: false
+    },
+    {
+        id: 34,
+        title: 'Wire-Rimmed Sunglasses',
+        desc: 'Posuere morbi leo urna molestie at elementum eu facilisis sed. At auctor urna nunc id cursus metus. Eu consequat ac felis donec et odio pellentesque.',
+        price: 40,
+        img: Item34,
+        category: 'accessories',
+        subcat: 'eyewear',
+        sale: false
+    },
+    {
+        id: 35,
+        title: 'Banks Sunglasses',
+        desc: 'Dui sapien eget mi proin sed libero. Pharetra pharetra massa massa ultricies. Nisi scelerisque eu ultrices vitae auctor eu augue. Blandit cursus risus at ultrices.',
+        price: 90,
+        img: Item35,
+        category: 'accessories',
+        subcat: 'eyewear',
+        sale: false
+    },
+    {
+        id: 36,
+        title: 'Belgrave Sunglasses',
+        desc: 'Eget nunc lobortis mattis aliquam faucibus purus. Nibh praesent tristique magna sit amet purus gravida quis.',
+        price: 40,
+        img: Item36,
+        category: 'accessories',
+        subcat: 'eyewear',
+        sale: false
+    },
+    {
+        id: 37,
+        title: 'Griffith Sunglasses',
+        desc: 'Quisque egestas diam in arcu. Amet risus nullam eget felis. Faucibus a pellentesque sit amet. Morbi leo urna molestie at elementum eu.',
+        price: 40,
+        img: Item37,
+        category: 'accessories',
+        subcat: 'eyewear',
+        sale: false
+    },
+    {
+        id: 38,
+        title: 'Pickfair Sunglasses',
+        desc: 'Quisque egestas diam in arcu. Amet risus nullam eget felis. Faucibus a pellentesque sit amet. Morbi leo urna molestie at elementum eu.',
+        price: 40,
+        img: Item38,
+        category: 'accessories',
+        subcat: 'eyewear',
+        sale: false
+    },
+    {
+        id: 39,
+        title: 'Boardwalk Ankle Sandals',
+        desc: 'Quisque egestas diam in arcu. Amet risus nullam eget felis. Faucibus a pellentesque sit amet. Morbi leo urna molestie at elementum eu.',
+        price: 80,
+        img: Item39,
+        category: 'shoes',
+        subcat: 'sandals',
+        sale: false
+    },
+    {
+        id: 40,
+        title: 'Knotted Slide Sandals',
+        desc: 'Quisque egestas diam in arcu. Amet risus nullam eget felis. Faucibus a pellentesque sit amet. Morbi leo urna molestie at elementum eu.',
+        price: 50,
+        img: Item40,
+        category: 'shoes',
+        subcat: 'sandals',
+        sale: false
+    },
+    {
+        id: 41,
+        title: 'Brooks Ankle Strap Sandals',
+        desc: 'Quisque egestas diam in arcu. Amet risus nullam eget felis. Faucibus a pellentesque sit amet. Morbi leo urna molestie at elementum eu.',
+        price: 120,
+        img: Item41,
+        category: 'shoes',
+        subcat: 'heels',
+        sale: false
+    },
+    {
+        id: 42,
+        title: 'Transport Tote in Corduroy',
+        desc: 'Quisque egestas diam in arcu. Amet risus nullam eget felis. Faucibus a pellentesque sit amet. Morbi leo urna molestie at elementum eu.',
+        price: 130,
+        img: Item42,
+        category: 'accessories',
+        subcat: 'purses',
         sale: false
     }
 ];
