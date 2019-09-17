9823db4c12b58e963131b5d02ce5e3b3e5c6f6dc
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-17 01:10:41 -0400

    Update Routes.js
    Add routing for sub-categories

--- src/constants/Routes.js
+++ src/constants/Routes.js
@@ -2,18 +2,47 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from '../components/Home';
 import Item from '../components/shopping/Item';
-import Clothing from '../components/shopping/clothing';
-import Shoes from '../components/shopping/shoes';
-import Accessories from '../components/shopping/accessories';
+
+import Clothing from '../components/shopping/clothing/clothing';
+import Tops from '../components/shopping/clothing/tops';
+import Sweaters from '../components/shopping/clothing/sweaters';
+import Jackets from '../components/shopping/clothing/jackets';
+import Denim from '../components/shopping/clothing/denim';
+import Dresses from '../components/shopping/clothing/dresses';
+
+import Shoes from '../components/shopping/shoes/shoes';
+import Boots from '../components/shopping/shoes/boots';
+import Sandals from '../components/shopping/shoes/sandals';
+
+import Accessories from '../components/shopping/accessories/accessories';
+import Purses from '../components/shopping/accessories/purses';
+import Jewelry from '../components/shopping/accessories/jewelry';
+import Glasses from '../components/shopping/accessories/glasses';
+
 import Cart from '../components/cart/Cart';
 import Contact from '../components/footer/contact/contact';
 
-export const Routes = <div>
-            <Route exact path="/" component={ Home } />
-            <Route path="/products" component={ Item } />
-            <Route path="/clothing" component={ Clothing } />
-            <Route path="/shoes" component={ Shoes } />
-            <Route path="/accessories" component={ Accessories } />
-            <Route path="/cart" component={ Cart } />
-            <Route path="/contact" component={ Contact } />
+export const Routes = 
+            <div>
+                <Route exact path="/" component={ Home } />
+
+                <Route path="/clothing" component={ Clothing } />
+                <Route path="/tops" component={ Tops } />
+                <Route path="/sweaters" component={ Sweaters } />
+                <Route path="/jackets" component={ Jackets } />
+                <Route path="/denim" component={ Denim } />
+                <Route path="/dresses" component={ Dresses } />
+
+                <Route path="/shoes" component={ Shoes } />
+                <Route path="/boots" component={ Boots } />
+                <Route path="/sandals" component={ Sandals } />
+
+                <Route path="/accessories" component={ Accessories } />
+                <Route path="/purses" component={ Purses } />
+                <Route path="/jewelry" component={ Jewelry } />
+                <Route path="/glasses" component={ Glasses } />
+
+                <Route path="/products" component={ Item } />
+                <Route path="/cart" component={ Cart } />
+                <Route path="/contact" component={ Contact } />
             </div>
