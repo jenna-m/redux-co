a11bee841d20be3f6ae11437a0dac1c404ecf9d4
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-17 17:31:55 -0400

    Update navbar.js
    Add dropdown menus

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -7,11 +7,34 @@ class Navbar extends React.Component {
             <nav className="navbar" role="navigation">
                 <div className="nav-container">
                     <NavLink exact to="/" className="brand-logo">redux Co.</NavLink>
-                    <ul className="menu">
+                    <ul class="menu">
                         <li><Link exact to="/">New Arrivals</Link></li>
-                        <li><Link to="/clothing">Clothing</Link></li>
-                        <li><Link to="/shoes">Shoes</Link></li>
-                        <li><Link to="/accessories">Accessories</Link></li>
+                        <li>
+                            <li className="menu-category"><Link to="/clothing">Clothing</Link></li>
+                            <ul className="dropdown">
+                                <li className="sublink"><Link to="/tops">Tops</Link></li>
+                                <li className="sublink"><Link to="/sweaters">Sweaters</Link></li>
+                                <li className="sublink"><Link to="/jackets">Jackets</Link></li>
+                                <li className="sublink"><Link to="/denim">Denim</Link></li>
+                                <li className="sublink"><Link to="/dresses">Dresses</Link></li>
+                                <li className="sublink"><Link to="/skirts">Skirts</Link></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <li className="menu-category"><Link to="/shoes">Shoes</Link></li>
+                            <ul className="dropdown">
+                                <li className="sublink"><Link to="/boots">Boots</Link></li>
+                                <li className="sublink"><Link to="/sandals">Sandals</Link></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <li className="menu-category"><Link to="/accessories">Accessories</Link></li>
+                            <ul className="dropdown">
+                                <li className="sublink"><Link to="/purses">Purses</Link></li>
+                                <li className="sublink"><Link to="/jewelry">Jewelry</Link></li>
+                                <li className="sublink"><Link to="/eyewear">Eyewear</Link></li>
+                            </ul>
+                        </li>
                         <li class="bag-ico"><NavLink to="/cart"><i class="fa fa-shopping-bag"></i></NavLink></li>
                     </ul>
                 </div>
