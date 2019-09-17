705f3fcc79e711d3baa599a598c436b8ed9ccef1
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-17 17:32:50 -0400

    Update Home.js
    Limit how many items appear on Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,21 +14,23 @@ const mapStateToProps = (state) => {
 
 class Home extends React.Component {
     render() {
-        let itemList = this.props.items.map(item =>{
-            return (
-                <div className="new-arrivals-item-card" key={item.id}>
-                    <div className="new-arrivals-item-image">
-                        <Link
-                            to = {{
-                                pathname: `/products/${item.category}/${item.subcat}`,
-                                search: `?id=${item.id}`,
-                            }}
-                            component={ Item }>
-                                <img src={item.img} alt={item.title} />
-                        </Link>
+        let itemList = this.props.items.map(item => {
+            if (item.id > 6) {
+                return (
+                    <div className="new-arrivals-item-card" key={item.id}>
+                        <div className="new-arrivals-item-image">
+                            <Link
+                                to = {{
+                                    pathname: `/products/${item.category}/${item.subcat}`,
+                                    search: `?id=${item.id}`,
+                                }}
+                                component={ Item }>
+                                    <img src={item.img} alt={item.title} />
+                            </Link>
+                        </div>
                     </div>
-                </div>
-            )
+                )
+            }
         })
 
         return (
