18453e78cc904d79fb63aaeedc3bc6676560360a
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-18 00:42:42 -0400

    Revamp Home component
    Add hero image and new messaging

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {
-    Link,
-    withRouter
-} from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import Item from './shopping/Item';
 
 const mapStateToProps = (state) => {
@@ -15,7 +12,7 @@ const mapStateToProps = (state) => {
 class Home extends React.Component {
     render() {
         let itemList = this.props.items.map(item => {
-            if (item.id > 6) {
+            if (item.id > 38) {
                 return (
                     <div className="new-arrivals-item-card" key={item.id}>
                         <div className="new-arrivals-item-image">
@@ -34,12 +31,22 @@ class Home extends React.Component {
         })
 
         return (
-            <div className="new-arrivals-container">
-                <h1>What's new?</h1>
-                <div className="new-arrivals-items">
-                    {itemList}
+            <div>
+                <div className="home-hero">
+                    <div className="home-hero-message">
+                        <h1>now open in Seattle!</h1>
+                        <Link className="hero-button" to="/new-arrivals">Shop the New Arrivals</Link>
+                    </div>
+                    <span>Photo by <a href="https://unsplash.com/@fng137">Felipe Galvan</a></span>
+                </div>
+                <div className="new-arrivals-container">
+                    <h1>How about...</h1>
+                    <div className="new-arrivals-items">
+                        {itemList}
+                    </div>
                 </div>
             </div>
+            
         );
     }
 }
