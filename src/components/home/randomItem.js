11c3489f5415d03425362cf2ff71c95a108b3514
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-18 12:34:17 -0400

    Create randomItem.js
    Create RandomItem component; component shows a randomly selected item with randomly generate messaging

---
+++ src/components/home/randomItem.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
+import { random } from 'lodash';
+import { numberOfItems } from '../../constants/ShopContent'; // Total # of items in shop
+import Item from '../shopping/item';
+
+const mapStateToProps = (state) => {
+    return {
+        items: state.items
+    };
+}
+
+class RandomItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: this.props.items
+        }
+    }
+
+    render() {
+        // Generate random messaging
+        let phrases = ['Looking for', 'How about', 'Need some', 'Searching for']
+        let randomNum = random(3);
+        let randomPhrase = phrases[randomNum];
+
+        // Find random item (based on ID)
+        let randomItemID = random(numberOfItems - 1);
+        let randomItem = this.props.items.map(item => {
+            if (item.id === randomItemID) {
+                return (
+                    <div className="random-item-container">
+                        <div className="random-item-message">
+                            <h1>{randomPhrase} {item.subcat}?</h1>
+                            <div className="see-more">
+                                <Link to={`/${item.subcat}`}>Browse all {item.subcat}</Link>
+                            </div>
+                        </div>
+                        <div className="random-item-card" key={item.id}>
+                            <div className="random-item-image">
+                                <Link to = {{
+                                        pathname: `/products/${item.category}/${item.subcat}`,
+                                        search: `?id=${item.id}`,
+                                    }}
+                                    component={ Item }>
+                                        <img src={item.img} alt={item.title} />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        })
+
+        return (
+            <div>
+                {randomItem}
+            </div> 
+        );
+    }
+}
+
+export default connect(mapStateToProps)(withRouter(RandomItem));
