813bb4f2ac89b202e008d9304cc889b6e5a40b15
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-18 00:30:45 -0400

    Update Empty.js
    Change <a> to <Link>

--- src/components/cart/Empty.js
+++ src/components/cart/Empty.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Empty extends React.Component {
     render() {
         return (
             <div className="empty-cart-message">
                 <p>Nothing</p>
-                <a className="button" href="/">Start shopping</a>
+                <Link className="button" to="/new-arrivals">Start shopping</Link>
             </div>
         );
     }
