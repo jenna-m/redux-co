1732166bbe7bd0405b59ca316461734ea1a1ef4c
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-22 15:47:26 -0400

    Update about.js

--- src/components/footer/about.js
+++ src/components/footer/about.js
@@ -12,7 +12,7 @@ class About extends React.Component {
                     It was my first time building an app with Redux, and I wanted to get more out of the tutorial than I originally had. I expanded upon it with the help of Google, StackOverflow, and GitHub. I've added item detail pages, revamped the cart display, implemented categories, took user experience and responsive design into consideration, added <a href="/404">404 routing</a>, among implementing other features to put my own spin on this online storefront.
                 </p>
                 <p>
-                    Keep in mind that this isn't a fully functioning online storefront (yet): there's a bug in the shopping cart (bag), there are no sizing options, the contact form isn't fully functional, and there is no backend or complete checkout process. I'm sure there are other problems that I haven't unearthed. Despite this, I'm extremely satisfied with the results and what I've learned so far. I plan to revisit this project in the future to add a backend and make it fully functional.
+                    Keep in mind that this isn't a fully functioning online storefront (yet): there's a bug in the shopping cart/bag, there are no sizing options, the contact form isn't fully functional, and there is no backend or complete checkout process. I'm sure there are other problems that I haven't unearthed. Despite this, I'm extremely satisfied with the results and what I've learned so far. I plan to revisit this project in the future to add a backend and make it fully functional.
                 </p>
                 <p>
                     All shop content (images & item names) was taken from <a href="https://madewell.com" target="new">Madewell</a>. Without their items, I would have had a pretty tough time curating a store.
@@ -28,6 +28,7 @@ class About extends React.Component {
                         <li><a href="https://lodash.com/" target="new">Lodash</a></li>
                         <li><a href="https://github.com/jaredpalmer/formik" target="new">Formik</a></li>
                         <li><a href="https://github.com/jquense/yup" target="new">Yup</a></li>
+                        <li><a href="https://fontawesome.com/" target="new">Font Awesome</a></li>
                         <li>And a whole lot of CSS</li>
                     </ul>
                 </p>
